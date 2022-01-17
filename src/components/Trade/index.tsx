571564dad22acd905df9a3b8a84ed1fe4c76973f
1148716206 <1148716206@qq.com>
2022-01-17 10:52:51 +0800

    t4

--- src/components/Trade/index.tsx
+++ src/components/Trade/index.tsx
@@ -10,10 +10,7 @@ const Trade = () => {
       <div>交易大厅</div>
       <div>交易大厅</div>
       git add .
-      git status
-      git commit -m 'XXX'
-      git push --set-upstream origin master
-      1
+
     </div>
   )
 }
