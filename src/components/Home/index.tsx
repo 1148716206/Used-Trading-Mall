2584ce39e60808980a1dd831291c5b48816476dd
1148716206 <1148716206@qq.com>
2022-01-18 09:33:53 +0800

    分页，模糊查询+新增

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import styles from './index.less'
+import request from '@/http';
 import {Input, Button, Carousel} from 'antd'
 import {SearchOutlined} from '@ant-design/icons'
 
 const Home = () => {
+  const [imagesData,setImagesData] = useState()
 
+  const loadDataSource = async () => {
+    const result: any = await request.get('api/getGoodsInfo')
+    console.log('result',result.data.msg[0].address)
+    setImagesData( result.data.msg[0].address)
+  };
+
+  useEffect(() => {
+    loadDataSource()
+  }, [])
   return (
     <div className={styles.wrapper}>
       <div className={styles.main}>
@@ -16,7 +27,7 @@ const Home = () => {
             <span className={styles.recommend__title__desc}>为你甄选最合适的</span>
           </div>
           <div className={styles.recommend__list}>
-
+            <img src={imagesData} alt=""/>
           </div>
         </div>
 
